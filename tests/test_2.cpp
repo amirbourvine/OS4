044490533a3fac6d9b0f4c7d0f508493bb1ad32b
Elad <elel290603@gmail.com>
2023-06-26 17:41:07 +0300

    Elad & Amir

--- tests/test_2.cpp
+++ tests/test_2.cpp
@@ -11,6 +11,7 @@ int main() {
 
     if (array1 == NULL) {
         std::cerr << "Memory allocation failed!\n";
+        return 1;
     }
 
     for (int i = 0; i < num_elements; i++) {
@@ -20,27 +21,38 @@ int main() {
     for (int i = 0; i < num_elements; i++) {
         if(array1[i] != 1){
             std::cerr << "Memory allocation failed!\n";
+            return 1;
         }
     }
 
     if(_num_free_blocks() != 0){
         std::cerr << "Incorrect number of free blocks\n";
+        return 1;
+
     }
 
     if(_num_free_bytes() != 0){
         std::cerr << "Incorrect number of free bytes\n";
+        return 1;
+
     }
 
     if(_num_allocated_blocks() != 1){
         std::cerr << "Incorrect number of allocated blocks\n";
+        return 1;
+
     }
 
     if(_num_allocated_bytes() != sizeof(int) * num_elements){
         std::cerr << "Incorrect number of allocated bytes\n";
+        return 1;
+
     }
     
     if(_num_meta_data_bytes() != _size_meta_data()){
         std::cerr << "Incorrect number of meta data bytes\n";
+        return 1;
+
     }
 
     std::cout << "allocation 1 succeed\n";
@@ -49,6 +61,8 @@ int main() {
 
     if (array2 == NULL) {
         std::cerr << "Memory allocation failed!\n";
+        return 1;
+
     }
 
     for (int i = 0; i < num_elements; i++) {
@@ -58,27 +72,39 @@ int main() {
     for (int i = 0; i < num_elements; i++) {
         if(array2[i] != 1){
             std::cerr << "Memory allocation failed!\n";
+            return 1;
+
         }
     }
 
     if(_num_free_blocks() != 0){
         std::cerr << "Incorrect number of free blocks\n";
+        return 1;
+
     }
 
     if(_num_free_bytes() != 0){
         std::cerr << "Incorrect number of free bytes\n";
+        return 1;
+
     }
 
     if(_num_allocated_blocks() != 2){
         std::cerr << "Incorrect number of allocated blocks\n";
+        return 1;
+
     }
 
     if(_num_allocated_bytes() != sizeof(int) * num_elements * 2){
         std::cerr << "Incorrect number of allocated bytes\n";
+        return 1;
+
     }
 
     if(_num_meta_data_bytes() != 2 * _size_meta_data()){
         std::cerr << "Incorrect number of meta data bytes\n";
+        return 1;
+
     }
 
     std::cout << "allocation 2 succeed\n";
@@ -87,6 +113,8 @@ int main() {
 
     if (array3 == NULL) {
         std::cerr << "Memory allocation failed!\n";
+        return 1;
+
     }
 
     for (int i = 0; i < num_elements; i++) {
@@ -96,30 +124,45 @@ int main() {
     for (int i = 0; i < num_elements; i++) {
         if(array3[i] != 1){
             std::cerr << "Memory allocation failed!\n";
+            return 1;
+
         }
     }
 
     if(_num_free_blocks() != 0){
         std::cerr << "Incorrect number of free blocks\n";
+        return 1;
+
     }
 
     if(_num_free_bytes() != 0){
         std::cerr << "Incorrect number of free bytes\n";
+        return 1;
+
     }
 
-    if(_num_allocated_blocks() != 2){
+    if(_num_allocated_blocks() != 3){
         std::cerr << "Incorrect number of allocated blocks\n";
+        return 1;
+
     }
 
     if(_num_allocated_bytes() != sizeof(int) * num_elements * 3){
         std::cerr << "Incorrect number of allocated bytes\n";
+        return 1;
+
     }
 
     if(_num_meta_data_bytes() != _size_meta_data() * 3){
         std::cerr << "Incorrect number of meta data bytes\n";
+        return 1;
+
     }
     
     std::cout << "allocation 3 succeed\n";
+
+    //Free tests
+    sfree(array1);
 
     return 0;
 }
