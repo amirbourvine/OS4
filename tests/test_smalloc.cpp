f60bdb9c351e0eb44c01af1bd358afc0ad1e682d
amirb <amirbourvine@campus.technion.ac.il>
2023-06-26 17:05:20 +0300

    h

--- tests/test_smalloc.cpp
+++ tests/test_smalloc.cpp
@@ -2,5 +2,14 @@
 // Created by amirb on 26/06/2023.
 //
 
+#include "header_2.h"
 
+int main(){
+    void* b1 = smalloc(10);
+    print();
+    void* b2 = smalloc(20);
+    print();
+    void* b3 = smalloc(30);
+    print();
+}
 
