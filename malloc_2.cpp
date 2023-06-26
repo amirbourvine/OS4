5d76ae2e1cd521c7c295618801840d1d85c09c62
amirb <amirbourvine@campus.technion.ac.il>
2023-06-26 14:07:14 +0300

    h

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -2,5 +2,19 @@
 // Created by amirb on 26/06/2023.
 //
 
+typedef struct MallocMetadata {
+    size_t size;
+    bool is_free;
+    MallocMetadata* next;
+    MallocMetadata* prev;
+} MallocMetadata;
+
+typedef struct BlocksList {
+    MallocMetadata* first;
+    unsigned int num_free_blocks;
+    unsigned int num_allocated_blocks;
+    unsigned int allocated_bytes; //does not include meta-data
+    unsigned int freed_bytes; //does not include meta-data
+} BlocksList;
 
 
