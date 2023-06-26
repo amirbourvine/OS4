69cbbf94bec9ac16a7a1396dc15c495421b6b2a7
Elad <elel290603@gmail.com>
2023-06-26 18:00:49 +0300

    Elad & Amir

--- tests/test_2.cpp
+++ tests/test_2.cpp
@@ -175,7 +175,7 @@ int main() {
 
     }
 
-    if(_num_allocated_blocks() != 2){
+    if(_num_allocated_blocks() != 3){
         std::cerr << "Incorrect number of allocated blocks\n";
         return 1;
 
