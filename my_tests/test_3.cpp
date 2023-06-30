c8b84a62aef83b51d48a8ff93755b8a81684a336
amirb <amirbourvine@campus.technion.ac.il>
2023-06-30 23:03:36 +0300

    debug

--- my_tests/test_3.cpp
+++ my_tests/test_3.cpp
@@ -3,7 +3,11 @@
 //
 #include "header_2.h"
 int main(){
+    std::cout << "HERE0\n";
+
     void* ptr1 = smalloc(16);  // Allocate 16 bytes
+
+    std::cout << "HERE1\n";
 
     void* ptr2 = smalloc(32);  // Allocate 32 bytes
 
@@ -13,11 +17,17 @@ int main(){
 //        overflowPtr[i] = 'A';
 //    }
 
+    std::cout << "HERE2\n";
+
     // Allocate more memory
     void* ptr3 = smalloc(64);  // Allocate 64 bytes
+
+    std::cout << "HERE3\n";
 
     // Free the allocations
     sfree(ptr1);
     sfree(ptr2);
     sfree(ptr3);
+
+    std::cout << "HERE4\n";
 }
